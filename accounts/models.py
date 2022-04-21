79c1dfe155c3ca09168f30765d4a096c6e68109d
Hexro <ngotanvn2001@gmail.com>
2022-04-21 20:42:02 +0700

    change model Customer (pt 5.3)

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,6 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name

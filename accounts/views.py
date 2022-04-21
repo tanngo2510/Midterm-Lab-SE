343c5b4fc1d0830a62c9e0d3043a4e6ac5726a41
Hexro <ngotanvn2001@gmail.com>
2022-04-22 01:59:21 +0700

    user registration (pt 14.1)

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from .models import *
 from .forms import *
 from .filters import *
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):

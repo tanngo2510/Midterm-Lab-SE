07fb7da6db2c3effb2da95b559f829f7e6f8e094
Hexro <ngotanvn2001@gmail.com>
2022-04-21 21:23:52 +0700

    show in dashboard (done pt 8)

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,17 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):

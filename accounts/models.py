dbe9dfaca31eb3f41fd223c1c4310257893f2387
Hexro <ngotanvn2001@gmail.com>
2022-04-21 20:54:06 +0700

    create Tag model (pt 6.1)

--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -23,6 +30,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -39,6 +47,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-
-    def __str__(self):
-        return self.product.name

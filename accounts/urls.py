0050415db912bc7bae28fd4d54290959a5bceb78
Hexro <ngotanvn2001@gmail.com>
2022-04-22 00:43:26 +0700

    UPDATE with Order Model (pt 10.2)

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/', views.customer, name='customer'),
     path('create_order/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
 ]

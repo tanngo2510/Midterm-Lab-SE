4568b57147d21ef0bc511b707583df756281adfc
Hexro <ngotanvn2001@gmail.com>
2022-04-21 20:49:27 +0700

    create Product & Order model & migrate (done pt 5)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import *
 
 admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)

24bbc631c8275947a244828381d18dfb59199190
Hexro <ngotanvn2001@gmail.com>
2022-04-23 16:36:55 +0700

    password reset email (pt 20)

--- crm/settings.py
+++ crm/settings.py
@@ -134,3 +134,12 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hexro99@gmail.com'
+EMAIL_HOST_PASSWORD = 'Nhattan2510'
